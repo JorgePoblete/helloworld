ca9458ddf3e1aaddbcbde0e380a7208fb5cf21b4
koke <jpobleteriquelme@gmail.com>
2021-12-05 22:52:36 -0300

    added prometheus

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
