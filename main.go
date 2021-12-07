a6bdcd975136bcf18ae67eefee11518c374e53fe
koke <jpobleteriquelme@gmail.com>
2021-12-06 23:02:06 -0300

    updated hello function to consume cpu

--- main.go
+++ main.go
@@ -4,20 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/healthcheck", healthcheck)
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	d, _ := time.ParseDuration("1s")
-	time.Sleep(d)
-	fmt.Fprintf(w, "Hello, world!!!\n")
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s", r.URL)
+	count := 1
+	for i := 0; i <= 1000000; i++ {
+		count = i
+	}
+	fmt.Fprintf(w, "Hello, world %d times\n", count)
 }
